d47c8b06a8d652f2021bed6320f23fbf0bcc9aa4
Satyajeet Kumar <161824864+satyaa6969@users.noreply.github.com>
2025-11-01 18:55:01 +0530

    Update App.jsx

--- mindscribe-frontend/src/App.jsx
+++ mindscribe-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Login from './components/Login';
-import Dashboard from './components/Dashboard'; // <-- Import Dashboard
+import Dashboard from './components/dashboard'; // <-- Import Dashboard
 import './App.css';
 
 function App() {
